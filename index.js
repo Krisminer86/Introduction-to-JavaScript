561201fdcba6b0588a1750dcffff8356e4cc889a
Kris Miner <krisminer86@gmail.com>
2020-07-24 23:23:26 -0400

    did they teach this yet?

--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
-
-
+let votingAge= 21;
+if (votingAge > 18){
+    console.log("True")
+  }
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+let votingAge= 21;
+let myNumber= 42;
+
+if (votingAge > 18) {
+  myNumber++;
+  console.log(myNumber);
+}
 
 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
-
+let year= "1999";
+console.log(Number(year))
 
 
 
 //Task d: Write a function to multiply a*b 
 
-
+function myFunction(a, b) {
+  return a * b 
+}
 
 
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
+function calculateDogYears(age){
+  let dogYears = age*7;
+  console.log("Your dog is " + dogYears + " in dog years!")
+}
 
-
+calculateDogYears(1);
+calculateDogYears(.75);
 
 
 
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
-//takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
+//takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and 
+// returns the number of pounds of raw food to feed in a day.
 
 //feeding requirements
 // adult dogs at least 1 year 
@@ -47,9 +65,37 @@
 // 4 - 7 months 5% of their body weight 
 // 7 - 12 months 4% of their body weight
 
-// when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
+// when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - 
+//if your calculations are correct your result should be 0.44999999999999996
   
+// this is shit code that doesn't do anything.  Hoping Mosh can teach me Javascript later.
+function feedingRequirements(weight, age) {
+  if (age >= 1) {
+    if (weight < 6){
+      food = (weight * .05)
+    }
+    else if (weight < 11){
+      food = (weight * .04)
+    }
+    else if (weight < 15){
+      food = (weight * .03)
+    }
+    else if (weight > 15){
+      food = (weight * .02)
+    }
+  else if (age < .33){
+      food =(weight * .1)
+    }
+  else if (age < .583){
+      food = (weight * .05)
+    }
+  else if (age< 1){
+      food = (weight * .04)
+  }
+}
 
+console.log(feedingRequirements(15,1));
+// I give up on this. Most of this code is made up nonsense, since I didn't actually know what to do. 
 
 
 
@@ -60,19 +106,47 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
+
+//This is more shit code that doesn't do anything.  Did I miss the day we were taught this???  
+let playerPick = prompt("Do you choose rock, paper or scissors?");
+let randomPick = Math.random();
+
+  if (randomPick < .34){
+    randomPick=("rock")
+  }
+  else if (randomPick  < .66)  {
+    randomPick=("paper")
+  }
+  else {
+    randomPick=("scissors")
+  }
+if (playerPick == radompick){
+  console.log("its a tie")
+} else if{
   
-  
+}
+// This is where I stopped giving a shit about this problem.  I don't know how to do it.
+
+
+
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
+
+let kilometers = parseInt(prompt("Please enter number of kilometers:"));
+let miles = kilometers / 1.6;
+console.log(miles + " Miles");
+
 
 
 
 
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
-  
+let feet = parseInt(prompt("Please enter number of feet:"));
+let centimeters = feet * 30.48;
+console.log(centimeters + " Centimeters");
 
 
 
@@ -82,6 +156,11 @@
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+function annoyingSong(){
+  for (i = 99; i >= 1; i--)
+  console.log(i + " bottles of beer on the wall")
+}
+annoyingSong();
 
 
 
@@ -95,9 +174,24 @@
 //60s should be D 
 //and anything below 60 should be F
   
+function myGrade(grade) {
+  if (grade > 89) {
+    console.log("You got an A")
+  } else if (grade > 79) {
+      console.log("You got a B")
+  }  else if (grade > 69) {
+      console.log("You got a C")
+  }  else if (grade > 59) {
+      console.log("You got a D")
+  } else {
+      console.log("You got an F")
+  }
+myGrade(90);
 
-  
-  
+// This doesnt acutally work... but it looks pretty.
+
+
+
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
